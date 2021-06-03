43fb76e240e1e80dbf040619d174dd4e972bb0fa
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-06-03 15:04:27 -0500

    Make script require no user input

--- rootfs/var/lib/dietpi/services/reflash_os.sh
+++ rootfs/var/lib/dietpi/services/reflash_os.sh
@@ -5,11 +5,11 @@
 
 	REFLASH_PATH=$(sed -n '/^[[:blank:]]*AUTO_SETUP_FLASH_OS=/{s/^[^=]*=//p;q}' /boot/dietpi.txt)
 
-	whiptail --yesno "Are you sure that you want to flash DietPi to $REFLASH_PATH? This will erase all data on that drive." 10 50 --title DietPi-OS-Reflash || exit 0
 	mount -ro remount /
 	echo '[ OK ] Mounted RootFS as read-only'
 	dd if=$(findmnt -Ufnro SOURCE -M /) of="$REFLASH_PATH" status=progress
 	echo "[ OK ] Flashed DietPi to $REFLASH_PATH"
-	whiptail --msgbox "Please remove your installation media, then press 'Ok'." 10 50 --title DietPi-OS-Reflash 
+	echo 'DietPi will reboot in 10 seconds, please remove your installation media'
+	sleep 10
 	reboot
 }
