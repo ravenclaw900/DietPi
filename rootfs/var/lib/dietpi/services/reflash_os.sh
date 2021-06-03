8791579751fa28c0604ec76f98dea0c94eec2629
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-06-03 15:00:51 -0500

    Remake to not use DietPi-Globals

--- rootfs/var/lib/dietpi/services/reflash_os.sh
+++ rootfs/var/lib/dietpi/services/reflash_os.sh
@@ -3,19 +3,13 @@
 	# Error out on command failures
 	set -e
 
-	# Load DietPi-Globals
-	. /boot/dietpi/func/dietpi-globals
-	readonly G_PROGRAM_NAME='DietPi-OS-Reflash'
-	G_CHECK_ROOT_USER
-
 	REFLASH_PATH=$(sed -n '/^[[:blank:]]*AUTO_SETUP_FLASH_OS=/{s/^[^=]*=//p;q}' /boot/dietpi.txt)
 
-	G_WHIP_YESNO "Are you sure that you want to flash DietPi to $REFLASH_PATH? This will erase all data on that drive." || exit 0
-	G_EXEC mount -ro remount /
-	G_DIETPI-NOTIFY 0 'Mounted RootFS as read-only'
-	G_DIETPI-NOTIFY -2 "Flashing DietPi to $REFLASH_PATH"
-	G_EXEC dd if=$(findmnt -Ufnro SOURCE -M /) of="$REFLASH_PATH"
-	G_DIETPI-NOTIFY 0 "Flashed DietPi to $REFLASH_PATH"
-	G_WHIP_MSG 'Please remove your installation media, then press OK.'
+	whiptail --yesno "Are you sure that you want to flash DietPi to $REFLASH_PATH? This will erase all data on that drive." 10 50 --title DietPi-OS-Reflash || exit 0
+	mount -ro remount /
+	echo '[ OK ] Mounted RootFS as read-only'
+	dd if=$(findmnt -Ufnro SOURCE -M /) of="$REFLASH_PATH" status=progress
+	echo "[ OK ] Flashed DietPi to $REFLASH_PATH"
+	whiptail --msgbox "Please remove your installation media, then press 'Ok'." 10 50 --title DietPi-OS-Reflash 
 	reboot
 }
